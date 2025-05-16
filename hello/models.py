51bfaba7f291eb03b5c132ff6b950f7d3a8dd064
kimsjava <kimsjava@gmail.com>
2025-05-16 15:28:19 +0900

    [C168]database,model,migration

--- hello/models.py
+++ hello/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Friend(models.Model):
+    name = models.CharField(max_length=100)
+    mail = models.EmailField(max_length=200)
+    age = models.IntegerField(default=0)
+    birthday = models.DateField()
+
+    def toString(self):
+        return f"Name: {self.name}, Mail: {self.mail}, Age: {self.age}, Birthday: {self.birthday}"
+    
+    def __str__(self):
+        return self.toString()
+    
