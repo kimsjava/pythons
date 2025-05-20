fe03cdf1df36693a2118eb0933b1576c75fd20fc
kimsjava <kimsjava@gmail.com>
2025-05-20 15:58:35 +0900

    [C2>130] Form>ChoiceFiledByJsonData

--- hello/forms.py
+++ hello/forms.py
@@ -8,5 +8,14 @@ class HelloForm(forms.Form):
     d1 = forms.DateField(label='date', widget=forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}))
     t1 = forms.TimeField(label='time', widget=forms.TimeInput(attrs={'class': 'form-control', 'type': 'time'}))
     dt1 = forms.DateTimeField(label='datetime', widget=forms.DateTimeInput(attrs={'class': 'form-control', 'type': 'datetime-local'}))
-    check = forms.BooleanField(label='check', required=False, widget=forms.CheckboxInput(attrs={'class': 'form-check-input'}))
+    #check = forms.BooleanField(label='check', required=False, widget=forms.CheckboxInput(attrs={'class': 'form-check-input'}))
+    check = forms.NullBooleanField(label='Check')
     
+    
+    data = [
+        ('one', 'item 1'),
+        ('two', 'item 2'),
+        ('three', 'item 3'),
+    ]
+    
+    choice = forms.ChoiceField(label='Choice', choices=data)
--- hello/views.py
+++ hello/views.py
@@ -10,23 +10,19 @@ class HelloView(TemplateView):
     def __init__(self):
         self.params = {
             'title': 'Hello',
-            'message': 'you date',
-            'd1':'',
-            't1':'',
-            'dt1':'',
+            # 'message': 'you date',
+            # 'd1':'',
+            # 't1':'',
+            # 'dt1':'',
             'form': HelloForm(),
             'result': None,
-            
         }
 
     def get(self, request):
         return render(request, 'hello/index.html', self.params)
     
     def post(self, request):
-        if('check' in request.POST):
-            self.params['result'] = 'Checked'
-        else:
-            self.params['result'] = 'Not Checked'
+        self.params['result'] = f'you selected:{request.POST.get('choice')}'
             
         self.params['form'] = HelloForm(request.POST)
         self.params['message'] = f'Hello {request.POST['name']}, your age is {request.POST['age']} and your mail {request.POST['mail']}'
