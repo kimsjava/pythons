28dd25514267033db95980f130dda0e9c2359635
kimsjava <kimsjava@gmail.com>
2025-05-27 17:21:57 +0900

    [C3>3-3>203P]QuerySet3,DTL(Django Template Language)연습

--- hello/views.py
+++ hello/views.py
@@ -2,23 +2,16 @@ from django.shortcuts import render  # render 함수 import 추가
 from .models import Friend
 from .forms import HelloForm
 
+def __new_str(friend):
+    if friend is None:
+        return ''
+    return f"id={friend.id} | name={friend.name} | age={friend.age}"
+
 # 함수형 뷰
 def index(request):
-    data = Friend.objects.all().values('id', 'name', 'age')
+    friendAll = Friend.objects.all()
     params = {
         'title': 'Hello',
-        'message': 'all friends',
-        'form':HelloForm(),
-        'data': data,
-        
+        'data': friendAll,
     }
-    
-    # if request.method == 'POST':
-    #     num = request.POST['id']
-    #     item = Friend.objects.get(id=num)
-    #     params['data'] = [item]
-    #     params['form'] = HelloForm(request.POST)
-    # else:
-    #     params['data'] = Friend.objects.all()
-        
     return render(request, 'hello/index.html', params)
